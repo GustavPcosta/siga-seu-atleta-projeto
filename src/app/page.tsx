13516c587c098b717ba767cf8cd166f9e447d98c
GustavPcosta <onepim@hotmail.com>
2025-03-13 14:44:38 -0300

    para-deply-netlfy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,25 @@ import { findAthletes } from '../lib/athlets';
 import { Suspense } from 'react';
 import Filters from './_components/filters';
 import { findSports } from '../lib/sport';
-import { PageProps } from 'next';
 
-export default async function Home({ searchParams }: PageProps) {
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: {
+    q?: string;
+    category?: 'all' | 'olympic' | 'paralympic';
+    sport?: string;
+    sort?: "followers" | "name" | "sport";
+    dir?: "desc" | "asc";
+  }
+}) {
   const searchText = searchParams?.q || "";
   const category = searchParams?.category || "all";
   const sport = searchParams?.sport || "";
   const sort = searchParams?.sort || "followers";
   const dir = searchParams?.dir || "desc";
-
+  
   const athletes = await findAthletes({
     searchText,
     category,
@@ -19,9 +29,9 @@ export default async function Home({ searchParams }: PageProps) {
     sort,
     dir
   });
-
+  
   const sports = await findSports();
-
+  
   return (
     <main className="p-4 flex flex-col gap-12">
       <Filters sports={sports} />
@@ -34,3 +44,46 @@ export default async function Home({ searchParams }: PageProps) {
     </main>
   );
 }
+
+
+
+
+
+
+
+// import AthletesList from './_components/athlets-list';
+// import { findAthletes } from '../lib/athlets';
+// import { Suspense } from 'react';
+// import Filters from './_components/filters';
+// import { findSports } from '../lib/sport';
+// import { PageProps } from 'next';
+
+// export default async function Home({ searchParams }: PageProps) {
+//   const searchText = searchParams?.q || "";
+//   const category = searchParams?.category || "all";
+//   const sport = searchParams?.sport || "";
+//   const sort = searchParams?.sort || "followers";
+//   const dir = searchParams?.dir || "desc";
+
+//   const athletes = await findAthletes({
+//     searchText,
+//     category,
+//     sport,
+//     sort,
+//     dir
+//   });
+
+//   const sports = await findSports();
+
+//   return (
+//     <main className="p-4 flex flex-col gap-12">
+//       <Filters sports={sports} />
+//       <Suspense key={searchText + category + sport + sort + dir} fallback={<div>Carregando...</div>}>
+//         <AthletesList
+//           filters={{ searchText, category, sport, sort, dir }}
+//           initialData={athletes}
+//         />
+//       </Suspense>
+//     </main>
+//   );
+// }
