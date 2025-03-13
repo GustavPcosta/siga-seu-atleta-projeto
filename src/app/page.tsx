36ebfb2ea1ba65959216886908ec60f8e9001f17
GustavPcosta <onepim@hotmail.com>
2025-03-13 15:53:30 -0300

    modicacao-page-tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,71 @@
+// import AthletesList from './_components/athlets-list';
+// import { findAthletes } from '../lib/athlets';
+// import { Suspense } from 'react';
+// import Filters from './_components/filters';
+// import { findSports } from '../lib/sport';
+
+
+// export default async function Home({
+//   searchParams,
+// }: {
+//   searchParams: {
+//     q?: string;
+//     category?: 'all' | 'olympic' | 'paralympic';
+//     sport?: string;
+//     sort?: "followers" | "name" | "sport";
+//     dir?: "desc" | "asc";
+//   }
+// }) {
+//   const searchText = searchParams?.q || "";
+//   const category = searchParams?.category || "all";
+//   const sport = searchParams?.sport || "";
+//   const sort = searchParams?.sort || "followers";
+//   const dir = searchParams?.dir || "desc";
+  
+//   const athletes = await findAthletes({
+//     searchText,
+//     category,
+//     sport,
+//     sort,
+//     dir
+//   });
+  
+//   const sports = await findSports();
+  
+//   return (
+//     <main className="p-4 flex flex-col gap-12">
+//       <Filters sports={sports} />
+//       <Suspense key={searchText + category + sport + sort + dir} fallback={<div>Carregando...</div>}>
+//         <AthletesList
+//           filters={{ searchText, category, sport, sort, dir }}
+//           initialData={athletes}
+//         />
+//       </Suspense>
+//     </main>
+//   );
+// }
+
+
+import { NextPage } from 'next';
 import AthletesList from './_components/athlets-list';
 import { findAthletes } from '../lib/athlets';
 import { Suspense } from 'react';
 import Filters from './_components/filters';
 import { findSports } from '../lib/sport';
 
+interface SearchParams {
+  q?: string;
+  category?: 'all' | 'olympic' | 'paralympic';
+  sport?: string;
+  sort?: 'followers' | 'name' | 'sport';
+  dir?: 'desc' | 'asc';
+}
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: {
-    q?: string;
-    category?: 'all' | 'olympic' | 'paralympic';
-    sport?: string;
-    sort?: "followers" | "name" | "sport";
-    dir?: "desc" | "asc";
-  }
-}) {
+interface HomeProps {
+  searchParams: SearchParams;
+}
+
+const Home: NextPage<HomeProps> = async ({ searchParams }) => {
   const searchText = searchParams?.q || "";
   const category = searchParams?.category || "all";
   const sport = searchParams?.sport || "";
@@ -43,10 +93,9 @@ export default async function Home({
       </Suspense>
     </main>
   );
-}
+};
 
-
-
+export default Home;
 
 
 
