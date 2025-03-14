2c2a51a8b430a24548e50042d38b6224586e093f
GustavPcosta <onepim@hotmail.com>
2025-03-14 17:25:39 -0300

    projeto-para-deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,3 @@
-// import AthletesList from './_components/athlets-list';
-// import { findAthletes } from '../lib/athlets';
-// import { Suspense } from 'react';
-// import Filters from './_components/filters';
-// import { findSports } from '../lib/sport';
-
-
-
-// export default async function Home({
-//   searchParams,
-// }: {
-//   searchParams: {
-//     q?: string;
-//     category?: "all" | "olympic" | "paralympic";
-//     sport?: string;
-//     sort?: "followers" | "name" | "sport";
-//     dir?: "desc" | "asc";
-//   };
-// }) {
-//   const searchText = searchParams.q || "";
-//   const category = searchParams.category || "all";
-//   const sport = searchParams.sport;
-//   const sort = searchParams.sort || "followers";
-//   const dir = searchParams.dir || "desc";
-
-//   const athletes = await findAthletes({
-//     searchText,
-//     category,
-//     sport,
-//     sort,
-//     dir,
-//   });
-
-//   const sports = await findSports();
-
-//   return (
-//     <main className="p-4 flex flex-col gap-12">
-//       <Filters sports={sports} />
-//       <Suspense
-//         key={searchText + category + sport + sort + dir}
-//         fallback={<div>Carregando...</div>}
-//       >
-//         <AthletesList
-//           filters={{ searchText, category, sport, sort, dir }}
-//           initialData={athletes}
-//         />
-//       </Suspense>
-//     </main>
-//   );
-// }
-
-
 
 import AthletesList from './_components/athlets-list';
 import { findAthletes } from '../lib/athlets';
@@ -68,7 +16,7 @@ export default async function Home({
     dir?: "desc" | "asc";
   };
 }) {
-  // Await searchParams to access its properties
+  
   const { q = "", category = "all", sport, sort = "followers", dir = "desc" } = await searchParams;
 
   const athletes = await findAthletes({
